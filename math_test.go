1a7432ae2aec7da69a16458effef2ea9d20cf8f9
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2022-10-02 21:48:04 -0300

    Update math_test.go

--- math_test.go
+++ math_test.go
@@ -3,17 +3,10 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
-	total := Soma(15, 15)
+
+	total := soma(15, 15)
 
 	if total != 30 {
-		t.Errorf("Resultado da soma é invalido: Resulatdo %d. Esperado: %d.", total, 30)
-	}
-}
-
-func TestSoma2(t *testing.T) {
-	total := Soma(1, 1)
-
-	if total != 2 {
-		t.Errorf("Resultado da soma é invalido: Resulatdo %d. Esperado: %d.", total, 30)
+		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
